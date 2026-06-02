b6f3c215b48734915375c6c6cdfa2baa455ff419
nettoyage-boreale <mhanou442@gmail.com>
2026-06-02 14:48:08 -0400

    fix allowed hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)40xfk)e2-vi6=ks6nwa!!zmuki!m(^!*!#qhf-sa!$o^njl72
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
